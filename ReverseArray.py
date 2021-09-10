ed694f92425d8a676db90ffee15fdab640a9dd1d
Sheikh Shamshad Ahmed <sheikh15-12126@diu.edu.bd>
2021-09-10 21:14:02 +0600

    reverse array

---
+++ ReverseArray.py
@@ -0,0 +1,13 @@
+def ReverseArray(A,start,end):
+    while start<end:
+        A[start],A[end]=A[end],A[start]
+    
+        start=start+1
+        end=end-1
+
+
+a=[1,2,3,4,5]
+n=len(a)
+ReverseArray(a,0,n-1)
+print(a)
+
